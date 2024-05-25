73123b5e53dfe7dae312f8aa00e7f6d8b3c05bb4
Onalerona Pako <onapako90@gmail.com>
2024-05-25 14:36:51 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# This Puppet manifest installs puppet-lint
+# This Puppet manifest install flask from pip3.
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
