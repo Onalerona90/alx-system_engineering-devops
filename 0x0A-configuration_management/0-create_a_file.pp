acdddf88afebeed6dc60f8fc35e63403760a4a0c
Onalerona Pako <onapako90@gmail.com>
2024-05-25 14:34:20 +0200

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # This Puppet manifest creates a file in /tmp
 
-file { 'holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
