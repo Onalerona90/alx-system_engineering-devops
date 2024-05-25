6fdf1c55a4854cbdbc240238636aec8908508eee
Onalerona Pako <onapako90@gmail.com>
2024-05-25 14:50:13 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/pup
 # Install flask from pip3
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
