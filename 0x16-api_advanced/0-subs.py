4dfa412c57e849e0a394415c2cdc994ee2a7a972
Onalerona Pako <onapako90@gmail.com>
2024-06-05 21:36:28 +0200

    Restructure code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Module for task 0"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    import requests
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     try:
